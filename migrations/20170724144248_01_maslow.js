bcc10de80e34dcee8bba11ae23bf2ec19f54fe48
Roxanne Baldwin <space.legs@Roxannes-MacBook-Air.local>
2017-07-24 14:56:48 -0600

    🦅 migration for table 01_maslow completed

--- migrations/20170724144248_01_maslow.js
+++ migrations/20170724144248_01_maslow.js
@@ -1,8 +1,15 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('maslow', (table) => {
+    table.increments()
+    table.text('shelter')
+    table.text('protection')
+    table.text('transportation')
+    table.integer('how many people in group')
+    table.boolean('infected?').notNullable()
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('maslow')
 };
