724986fa5c09c4ea433dc1a202e1c6270d0e6823
Roxanne Baldwin <space.legs@Roxannes-MacBook-Air.local>
2017-07-29 16:27:03 -0600

    👩🏼‍💻

--- routes/breather_maslow.js
+++ routes/breather_maslow.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const queries = require('../queries')
 const knex = require('knex')
 
-router.get('/meatdecoy', (request, response) => {
+router.get('/meatdecoy/', (request, response) => {
   queries.getAll()
   .then(function(maslow){
     response.json(maslow)
@@ -17,7 +17,7 @@ router.get('/meatdecoy/:id', (request, response) => {
   })
 })
 
-router.post('/meatdecoy', (request, response) => {
+router.post('/meatdecoy/', (request, response) => {
   var body = request.body
   queries.postNew(body)
   .then(function(message){
